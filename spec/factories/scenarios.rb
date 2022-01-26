c8e769963c6532aa468d2d238e31f86c40fcdbf9
Solton33 <mid.night1202@gmail.com>
2022-01-26 17:09:51 +0900

    テストコードの実行

--- spec/factories/scenarios.rb
+++ spec/factories/scenarios.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :scenario do
-    
+        title {Faker::Books::Lovecraft.words}
+        scenario {Faker::Books::Lovecraft.paragraphs}
+        association :user
+
+        after(:build) do |scenario|
+          scenario.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+        end
   end
 end
